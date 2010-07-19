c9e3b921b8ce2021030efc718a538e60afb1b9ed
Abel Sinkovics <abelsoft@freemail.hu>
2010-07-19 23:11:33 +0200

    Adding test cases for compose

--- src/lib/mpllibs/util/test/compose.cpp
+++ src/lib/mpllibs/util/test/compose.cpp
@@ -5,3 +5,78 @@
 
 #include <mpllibs/util/compose.h>
 
+#include <mpllibs/test/test.h>
+#include <mpllibs/test/TestSuite.h>
+
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/plus.hpp>
+#include <boost/mpl/times.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+namespace
+{
+  const mpllibs::test::TestSuite suite("compose");
+
+  struct make_pointer
+  {
+    template <class T>
+    struct apply : boost::mpl::identity<T*> {};
+  };
+  
+  struct incr
+  {
+    template <class T>
+    struct apply : boost::mpl::plus<boost::mpl::int_<13>, T> {};
+  };
+
+  struct double_value
+  {
+    template <class T>
+    struct apply : boost::mpl::times<boost::mpl::int_<2>, T> {};
+  };
+
+  typedef
+    boost::is_same<
+      boost::mpl::apply<
+        mpllibs::util::compose<make_pointer, make_pointer>,
+        int
+      >::type,
+      int**
+    >
+    TestSameFunctionTwice;
+
+  typedef
+    boost::is_same<
+      boost::mpl::apply<
+        mpllibs::util::compose<
+          make_pointer,
+          make_pointer,
+          make_pointer,
+          make_pointer,
+          make_pointer
+        >,
+        int
+      >::type,
+      int*****
+    >
+    TestSameFunctionFiveTimes;
+
+  typedef
+    boost::mpl::equal_to<
+      boost::mpl::apply<
+        mpllibs::util::compose<double_value, incr>,
+        boost::mpl::int_<1>
+      >::type,
+      boost::mpl::int_<28>
+    >
+    TestOrder;
+
+}
+
+MPLLIBS_ADD_TEST(suite, TestSameFunctionTwice)
+MPLLIBS_ADD_TEST(suite, TestSameFunctionFiveTimes)
+MPLLIBS_ADD_TEST(suite, TestOrder)
+
+
