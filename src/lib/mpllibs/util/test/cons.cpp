79821d5bbaab19ef77496c7b1b78a6f5785aa46e
Abel Sinkovics <abelsoft@freemail.hu>
2010-07-20 07:53:54 +0200

    Adding test to cons

--- src/lib/mpllibs/util/test/cons.cpp
+++ src/lib/mpllibs/util/test/cons.cpp
@@ -5,3 +5,43 @@
 
 #include <mpllibs/util/cons.h>
 
+#include <mpllibs/test/equal_sequence.h>
+#include <mpllibs/test/test.h>
+#include <mpllibs/test/TestSuite.h>
+
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/pair.hpp>
+
+namespace
+{
+  const mpllibs::test::TestSuite suite("cons");
+  
+  typedef boost::mpl::int_<11> int11;
+  typedef boost::mpl::int_<13> int13;
+
+  typedef
+    mpllibs::test::equal_sequence<
+      boost::mpl::apply<
+        mpllibs::util::cons,
+        boost::mpl::pair<int11, boost::mpl::list<int13> >
+      >::type,
+      boost::mpl::list<int11, int13>
+    >
+    TestPushingToList;
+
+  typedef
+    mpllibs::test::equal_sequence<
+      boost::mpl::apply<
+        mpllibs::util::cons,
+        boost::mpl::pair<int13, boost::mpl::list< > >
+      >::type,
+      boost::mpl::list<int13>
+    >
+    TestPushingToEmptyList;
+}
+
+MPLLIBS_ADD_TEST(suite, TestPushingToList)
+MPLLIBS_ADD_TEST(suite, TestPushingToEmptyList)
+
