dc7ca7c1b05560a3a10441b021ec7bcfa2adfa34
Abel Sinkovics <abelsoft@freemail.hu>
2010-08-21 12:53:25 +0200

    Use HAS_XXX_TRAIT_DEF from boost

--- src/lib/mpllibs/test/has_type.h
+++ src/lib/mpllibs/test/has_type.h
@@ -6,48 +6,13 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <mpllibs/test/yes.h>
-#include <mpllibs/test/no.h>
-
-#include <boost/mpl/apply.hpp>
-#include <boost/mpl/always.hpp>
+#include <boost/mpl/has_xxx.hpp>
 
 namespace mpllibs
 {
   namespace test
   {
-    // Based on C++ Template Metaprogramming, chapter 9.10
-    template <class F>
-    struct has_type
-    {
-      struct type
-      {
-        // It needs an extra int argument to remove ambiguity between
-        // this version and the one with ...
-        template <class T>
-        static
-          typename
-            boost::mpl::apply<
-              boost::mpl::always<mpllibs::test::yes>,
-              typename T::type
-            >::type
-          tester(T*, int);
-      
-        // I have to pass a T* argument to make Visual C++ accept it
-        template <class T>
-        static mpllibs::test::no tester(T*, ...);
-      
-        static const bool
-          value =
-            sizeof(
-              mpllibs::test::has_type<F>::type::tester(
-                static_cast<F*>(0),
-                13
-              )
-            )
-            == sizeof(mpllibs::test::yes);
-      };
-    };
+    BOOST_MPL_HAS_XXX_TRAIT_DEF(type)
   }
 }
 
