d47a8033468174578b4e942fcda5aae2dcec3ce8
Abel Sinkovics <abelsoft@freemail.hu>
2010-11-01 12:49:25 +0100

    Adding nth_of

---
+++ src/lib/mpllibs/metaparse/nth_of.h
@@ -0,0 +1,139 @@
+#ifndef MPLLIBS_PARSER_NTH_OF_H
+#define MPLLIBS_PARSER_NTH_OF_H
+
+// Copyright Abel Sinkovics (abel@sinkovics.hu)  2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metaparse/sequence.h>
+
+#include <mpllibs/metaparse/util/unless_nothing.h>
+
+#include <boost/mpl/at.hpp>
+#include <boost/mpl/pair.hpp>
+#include <boost/mpl/apply.hpp>
+
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/comma_if.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_params_with_a_default.hpp>
+#include <boost/preprocessor/arithmetic/sub.hpp>
+
+namespace mpllibs
+{
+  namespace metaparse
+  {
+    namespace impl
+    {
+      template <int n, class seq>
+      struct get_nth :
+        boost::mpl::at_c<
+          typename boost::mpl::first<typename seq::type>::type,
+          n
+        >
+      {};
+      
+      template <int n, class seq>
+      struct nth_of_c_impl
+      {
+        template <class s>
+        struct apply :
+          mpllibs::metaparse::util::unless_nothing<
+            boost::mpl::apply<seq, s>,
+            mpllibs::metaparse::util::make_pair<
+              mpllibs::metaparse::impl::get_nth<n, boost::mpl::apply<seq, s> >,
+              boost::mpl::second<typename boost::mpl::apply<seq, s>::type>
+            >
+          >
+        {};
+      };
+    }
+  
+    #ifdef NTH_OF_CASE
+      #error NTH_OF_CASE already defined
+    #endif
+    #define NTH_OF_CASE(z, n, unused) \
+      template < \
+        int k BOOST_PP_COMMA_IF(n) \
+        BOOST_PP_ENUM_PARAMS(n, class p) \
+      > \
+      struct nth_of_c##n : \
+        mpllibs::metaparse::impl::nth_of_c_impl< \
+          k, \
+          mpllibs::metaparse::sequence<BOOST_PP_ENUM_PARAMS(n, p)> \
+        > \
+      {};
+    
+    BOOST_PP_REPEAT(SEQUENCE_MAX_ARGUMENT, NTH_OF_CASE, ~)
+    
+    #undef NTH_OF_CASE
+    
+    
+
+
+
+
+    template <
+      int n,
+      BOOST_PP_ENUM_PARAMS_WITH_A_DEFAULT(
+        SEQUENCE_MAX_ARGUMENT,
+        class p,
+        mpllibs::metaparse::impl::sequence_no_argument
+      )
+    >
+    struct nth_of_c;
+
+
+    #ifdef SEQUENCE_UNUSED_PARAM
+      #error SEQUENCE_UNUSED_PARAM already defined
+    #endif
+    #define SEQUENCE_UNUSED_PARAM(z, n, unused) \
+      BOOST_PP_COMMA_IF(n) mpllibs::metaparse::impl::sequence_no_argument
+    
+    #ifdef NTH_OF_N
+      #error NTH_OF_N already defined
+    #endif
+    #define NTH_OF_N(z, n, unused) \
+      template <int k BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS(n, class p)> \
+      struct nth_of_c< \
+        k, \
+        BOOST_PP_ENUM_PARAMS(n, p) \
+        BOOST_PP_COMMA_IF(n) \
+        BOOST_PP_REPEAT( \
+          BOOST_PP_SUB(SEQUENCE_MAX_ARGUMENT, n), \
+          SEQUENCE_UNUSED_PARAM, \
+          ~ \
+        ) \
+      > : \
+        mpllibs::metaparse::nth_of_c##n< \
+          k BOOST_PP_COMMA_IF(n) \
+          BOOST_PP_ENUM_PARAMS(n, p) \
+        > \
+      {};
+    
+    BOOST_PP_REPEAT(SEQUENCE_MAX_ARGUMENT, NTH_OF_N, ~)
+    
+    #undef NTH_OF_N
+    #undef SEQUENCE_UNUSED_PARAM
+    
+    
+    
+    
+    
+    template <
+      class k,
+      BOOST_PP_ENUM_PARAMS_WITH_A_DEFAULT(
+        SEQUENCE_MAX_ARGUMENT,
+        class p,
+        mpllibs::metaparse::impl::sequence_no_argument
+      )
+    >
+    struct nth_of :
+      nth_of_c<k::type::value, BOOST_PP_ENUM_PARAMS(SEQUENCE_MAX_ARGUMENT, p)>
+    {};
+  }
+}
+
+#endif
+
---
+++ src/lib/mpllibs/metaparse/test/nth_of.cpp
@@ -0,0 +1,67 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metaparse/nth_of.h>
+
+#include "common.h"
+
+#include <mpllibs/metatest/test.h>
+#include <mpllibs/metatest/TestSuite.h>
+
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/int.hpp>
+
+namespace
+{
+  const mpllibs::metatest::TestSuite suite("nth_of");
+
+  typedef
+    boost::mpl::equal_to<
+      boost::mpl::apply<
+        mpllibs::metaparse::nth_of_c<0, lit_h>,
+        str_hello
+      >::type::first,
+      char_h
+    >
+    TestFirstOfOne;
+
+  typedef
+    boost::mpl::equal_to<
+      boost::mpl::apply<
+        mpllibs::metaparse::nth_of_c<0, lit_h, lit_e>,
+        str_hello
+      >::type::first,
+      char_h
+    >
+    TestFirstOfTwo;
+
+  typedef
+    boost::mpl::equal_to<
+      boost::mpl::apply<
+        mpllibs::metaparse::nth_of<boost::mpl::int_<1>, lit_h, lit_e>,
+        str_hello
+      >::type::first,
+      char_e
+    >
+    TestSecondOfTwo;
+
+  typedef
+    boost::mpl::equal_to<
+      boost::mpl::apply<
+        mpllibs::metaparse::nth_of_c<1, lit_x, lit_e>,
+        str_hello
+      >::type,
+      mpllibs::metaparse::nothing
+    >
+    TestNothing;
+}
+
+MPLLIBS_ADD_TEST(suite, TestFirstOfOne)
+MPLLIBS_ADD_TEST(suite, TestFirstOfTwo)
+MPLLIBS_ADD_TEST(suite, TestSecondOfTwo)
+MPLLIBS_ADD_TEST(suite, TestNothing)
+
+
