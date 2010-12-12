92efb23161ed997311c7b9a2db162b49a0794956
Abel Sinkovics <abelsoft@freemail.hu>
2010-12-12 16:32:45 +0100

    Reorder header files

--- src/lib/mpllibs/error/letrec.h
+++ src/lib/mpllibs/error/letrec.h
@@ -1,9 +1,10 @@
+#ifndef MPLLIBS_ERROR_LETREC_H
+#define MPLLIBS_ERROR_LETREC_H
+
 // Copyright Abel Sinkovics (abel@sinkovics.hu)  2010.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
-#ifndef MPLLIBS_ERROR_LETREC_H
-#define MPLLIBS_ERROR_LETREC_H
 
 #include <mpllibs/error/let.h>
 
--- src/lib/mpllibs/error/util/id.h
+++ src/lib/mpllibs/error/util/id.h
@@ -1,9 +1,10 @@
+#ifndef MPLLIBS_ERROR_UTIL_ID_H
+#define MPLLIBS_ERROR_UTIL_ID_H
+
 // Copyright Abel Sinkovics (abel@sinkovics.hu)  2010.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
-#ifndef MPLLIBS_ERROR_UTIL_ID_H
-#define MPLLIBS_ERROR_UTIL_ID_H
 
 namespace mpllibs
 {
