cf7b39babade930ffff5366ec8f1d1cbea80139b
Abel Sinkovics <abelsoft@freemail.hu>
2010-12-28 19:59:34 +0100

    Add unit test for return_

--- src/lib/mpllibs/error/test/return_.cpp
+++ src/lib/mpllibs/error/test/return_.cpp
@@ -5,4 +5,50 @@
 
 #include <mpllibs/error/return_.h>
 
+#include <mpllibs/metatest/test.h>
+#include <mpllibs/metatest/TestSuite.h>
+
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/identity.hpp>
+
+#include "common.h"
+
+namespace
+{
+  struct test_tag;
+  
+  struct Test
+  {
+    typedef test_tag tag;
+  };
+}
+
+namespace mpllibs
+{
+  namespace error
+  {
+    template <>
+    struct return__impl<test_tag>
+    {
+      template <class t>
+      struct apply : boost::mpl::identity<int13> {};
+    };
+  }
+}
+
+namespace
+{
+  const mpllibs::metatest::TestSuite suite("return_");
+
+  typedef
+    boost::mpl::equal_to<
+      int13,
+      boost::mpl::apply<mpllibs::error::return_<Test>, int>::type
+    >
+    TestSpecialisationIsCalled;  
+}
+
+MPLLIBS_ADD_TEST(suite, TestSpecialisationIsCalled)
+
 
