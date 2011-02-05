d93318495e0266612180e434ebc10f07fcac7d87
Abel Sinkovics <abelsoft@freemail.hu>
2011-02-05 16:48:48 +0100

    Adding compile-time try-catch

---
+++ src/lib/mpllibs/error/test/try_.cpp
@@ -0,0 +1,152 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/error/try_.h>
+#include <mpllibs/error/throw.h>
+
+#include <mpllibs/error/get_data.h>
+
+#include <mpllibs/metatest/test.h>
+#include <mpllibs/metatest/TestSuite.h>
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/always.hpp>
+#include <boost/mpl/identity.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+
+#include "common.h"
+
+namespace
+{
+  const mpllibs::metatest::TestSuite suite("try_");
+  
+  struct tag1
+  {
+    typedef tag1 type;
+  };
+  
+  struct e1
+  {
+    typedef tag1 tag;
+    typedef e1 type;
+  };
+  
+  struct tag2
+  {
+    typedef tag1 type;
+  };
+  
+  struct e2
+  {
+    typedef tag2 tag;
+    typedef e2 type;
+  };
+  
+  typedef
+    boost::mpl::equal_to<
+      int13,
+      TRY<
+        RETURN<mpllibs::error::ExceptionMonad, int13>
+      >
+      ::catch_<tag1, x, int11>
+      ::type
+    >
+    TestNoException;
+
+  typedef
+    boost::mpl::equal_to<
+      int11,
+      TRY<
+        THROW<e1>
+      >
+      ::catch_<tag1, x, boost::mpl::identity<int11> >
+      ::type
+    >
+    TestCatch;
+
+  typedef
+    boost::mpl::equal_to<
+      int13,
+      TRY<
+        THROW<int13>
+      >
+      ::catch_<boost::mpl::tag<int13>::type, x, boost::mpl::identity<x> >
+      ::type
+    >
+    TestExceptionValueInCatch;
+
+  typedef
+    boost::mpl::equal_to<
+      mpllibs::error::Exception<int13>,
+      TRY<
+        THROW<int13>
+      >
+      ::catch_<tag2, x, boost::mpl::identity<int11> >
+      ::type
+    >
+    TestNotCatching;
+
+  typedef
+    boost::mpl::equal_to<
+      int13,
+      TRY<
+        THROW<e2>
+      >
+      ::catch_<tag1, x, boost::mpl::identity<int11> >
+      ::catch_<tag2, x, boost::mpl::identity<int13> >
+      ::type
+    >
+    TestSecondCatch;
+
+  typedef
+    boost::mpl::equal_to<
+      int11,
+      TRY<
+        THROW<e1>,
+        RETURN<mpllibs::error::ExceptionMonad, int1>
+      >
+      ::catch_<tag1, x, boost::mpl::identity<int11> >
+      ::type
+    >
+    TestExceptionPropagation;
+
+
+  typedef
+    boost::mpl::equal_to<
+      int11,
+      TRY<
+        RETURN<mpllibs::error::ExceptionMonad, int13>,
+        RETURN<mpllibs::error::ExceptionMonad, int11>
+      >
+      ::catch_<tag1, x, boost::mpl::identity<int11> >
+      ::type
+    >
+    TestExecutionSequence;
+  
+  typedef
+    boost::mpl::equal_to<
+      int11,
+      TRY<
+        SET<x, THROW<e1> >,
+        RETURN<mpllibs::error::ExceptionMonad, int1>
+      >
+      ::catch_<tag1, x, boost::mpl::identity<int11> >
+      ::type
+    >
+    TestExceptionInSet;
+}
+
+MPLLIBS_ADD_TEST(suite, TestNoException)
+MPLLIBS_ADD_TEST(suite, TestCatch)
+MPLLIBS_ADD_TEST(suite, TestExceptionValueInCatch)
+MPLLIBS_ADD_TEST(suite, TestNotCatching)
+MPLLIBS_ADD_TEST(suite, TestSecondCatch)
+MPLLIBS_ADD_TEST(suite, TestExceptionPropagation)
+MPLLIBS_ADD_TEST(suite, TestExecutionSequence)
+MPLLIBS_ADD_TEST(suite, TestExceptionInSet)
+
+
---
+++ src/lib/mpllibs/error/try_.h
@@ -0,0 +1,92 @@
+#ifndef MPLLIBS_ERROR_TRY_H
+#define MPLLIBS_ERROR_TRY_H
+
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/error/do_.h>
+#include <mpllibs/error/let.h>
+#include <mpllibs/error/Exception.h>
+#include <mpllibs/error/get_data.h>
+
+#include <boost/mpl/tag.hpp>
+#include <boost/mpl/if.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+
+namespace mpllibs
+{
+  namespace error
+  {
+    #ifdef TRY
+      #error TRY already defined
+    #endif
+    #define TRY mpllibs::error::try_
+
+    namespace impl
+    {
+      // evaluates result lazily
+      template <class result>
+      struct skip_further_catches : result
+      {
+        template <class exception_tag, class name, class body>
+        struct catch_ : skip_further_catches {};
+      };
+      
+      template <class exception>
+      struct was_exception
+      {
+        typedef exception type;
+        
+        typedef mpllibs::error::get_data<exception> _exception_data;
+        
+        template <class exception_tag, class name, class body>
+        struct catch_ :
+          boost::mpl::if_<
+            boost::is_same<
+              exception_tag,
+              typename boost::mpl::tag<typename _exception_data::type>::type
+            >,
+            mpllibs::error::impl::skip_further_catches<
+              typename mpllibs::error::let<
+                name, typename _exception_data::type,
+                body
+              >::type
+            >,
+            mpllibs::error::impl::was_exception<exception>
+          >::type
+        {};
+      };
+    }
+
+    template <
+      BOOST_PP_ENUM_PARAMS_WITH_A_DEFAULT(
+        DO_MAX_ARGUMENT,
+        class e,
+        mpllibs::error::unused_do_argument
+      )
+    >
+    struct try_ :
+      boost::mpl::if_<
+        typename boost::is_same<
+          mpllibs::error::Exception_tag,
+          typename boost::mpl::tag<
+            typename DO<BOOST_PP_ENUM_PARAMS(DO_MAX_ARGUMENT, e)>::type
+          >::type
+        >::type,
+        mpllibs::error::impl::was_exception<
+          typename DO<BOOST_PP_ENUM_PARAMS(DO_MAX_ARGUMENT, e)>::type
+        >,
+        mpllibs::error::impl::skip_further_catches<
+          // NoException evaluates to the wrapped value
+          typename DO<BOOST_PP_ENUM_PARAMS(DO_MAX_ARGUMENT, e)>::type
+        >
+      >::type
+    {};
+  }
+}
+
+#endif
+
