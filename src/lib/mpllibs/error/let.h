fdaf53f188e589708d8f5dc4350e19ab2590be65
Abel Sinkovics <abelsoft@freemail.hu>
2011-02-05 18:56:29 +0100

    Simplify let's implementation

--- src/lib/mpllibs/error/let.h
+++ src/lib/mpllibs/error/let.h
@@ -51,41 +51,29 @@ namespace mpllibs
       #error LET_CLASS alread defined
     #endif
     #define LET_CLASS(z, n, unused) \
-      , class
+      BOOST_PP_COMMA_IF(n) class
     
     #ifdef LET_REC_CASE
       #error LET_REC_CASE alread defined
     #endif
     #define LET_REC_CASE(z, n, unused) \
-      BOOST_PP_COMMA_IF(n) typename let<a, e1, x##n>::type
+      BOOST_PP_COMMA_IF(n) typename mpllibs::error::let<a, e1, x##n>::type
 
     #ifdef LET_TEMPLATE_CASE
       #error LET_TEMPLATE_CASE already defined
     #endif
-    // I need at least one template argument. The "n"th case handles n+1 args.
     #define LET_TEMPLATE_CASE(z, n, unused) \
       template < \
         class a, \
         class e1, \
-        template<class BOOST_PP_REPEAT(n, LET_CLASS, ~) > class t, \
-        class y BOOST_PP_COMMA_IF(n) \
+        template<BOOST_PP_REPEAT(n, LET_CLASS, ~) > class t, \
         BOOST_PP_ENUM_PARAMS(n, class x) \
       > \
-      struct \
-        let_impl< \
-          a, \
-          e1, \
-          t<y BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS(n, x)> \
-        > : \
-          mpllibs::error::util::id< \
-            t< \
-              typename let<a, e1, y>::type BOOST_PP_COMMA_IF(n) \
-              BOOST_PP_REPEAT(n, LET_REC_CASE, ~) \
-            > \
-          > \
+      struct let_impl<a, e1, t<BOOST_PP_ENUM_PARAMS(n, x)> > : \
+        mpllibs::error::util::id< t< BOOST_PP_REPEAT(n, LET_REC_CASE, ~) > > \
       {};
     
-    BOOST_PP_REPEAT(LET_MAX_TEMPLATE_ARGUMENT, LET_TEMPLATE_CASE, ~)
+    BOOST_PP_REPEAT_FROM_TO(1, LET_MAX_TEMPLATE_ARGUMENT, LET_TEMPLATE_CASE, ~)
 
     #undef LET_TEMPLATE_CASE
     #undef LET_CLASS
