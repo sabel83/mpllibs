e437c51d893999b5439ddd6ada92f8262b4a4afc
Endre Tamas SAJO <sajobandi@gmail.com>
2011-04-21 00:10:31 +0200

    +a couple of tests
    rewritten from the boost::mpl unit tests

---
+++ src/sample/boost_mpl_unit_test/comparison.cpp
@@ -0,0 +1,68 @@
+// Copyright Endre Tamas SAJO (baja@inf.elte.hu) 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_2_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metatest/test.h>
+
+#include <boost/mpl/comparison.hpp>
+#include <boost/mpl/int.hpp>
+
+namespace
+{
+  const mpllibs::metatest::TestSuite suite ("comparison");
+
+  using namespace boost::mpl;
+
+  typedef int_<0> _0;
+  typedef int_<10> _10;
+
+  typedef equal_to <_0, _10> test_equal_to_1;
+  typedef equal_to <_10, _0> test_equal_to_2;
+  typedef equal_to <_10, _10> test_equal_to_3;
+
+  typedef not_equal_to <_0, _10> test_not_equal_to_1;
+  typedef not_equal_to <_10, _0> test_not_equal_to_2;
+  typedef not_equal_to <_10, _10> test_not_equal_to_3;
+
+  typedef less <_0, _10> test_less_1;
+  typedef less <_10, _0> test_less_2;
+  typedef less <_10, _10> test_less_3;
+
+  typedef less_equal <_0, _10> test_less_equal_1;
+  typedef less_equal <_10, _0> test_less_equal_2;
+  typedef less_equal <_10, _10> test_less_equal_3;
+
+  typedef greater <_0, _10> test_greater_1;
+  typedef greater <_10, _0> test_greater_2;
+  typedef greater <_10, _10> test_greater_3;
+
+  typedef greater_equal <_0, _10> test_greater_equal_1;
+  typedef greater_equal <_10, _0> test_greater_equal_2;
+  typedef greater_equal <_10, _10> test_greater_equal_3;
+}
+
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_equal_to_1)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_equal_to_2)
+MPLLIBS_ADD_TEST(suite, test_equal_to_3)
+
+MPLLIBS_ADD_TEST(suite, test_not_equal_to_1)
+MPLLIBS_ADD_TEST(suite, test_not_equal_to_2)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_not_equal_to_3)
+
+MPLLIBS_ADD_TEST(suite, test_less_1)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_less_2)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_less_3)
+
+MPLLIBS_ADD_TEST(suite, test_less_equal_1)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_less_equal_2)
+MPLLIBS_ADD_TEST(suite, test_less_equal_3)
+
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_greater_1)
+MPLLIBS_ADD_TEST(suite, test_greater_2)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_greater_3)
+
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test_greater_equal_1)
+MPLLIBS_ADD_TEST(suite, test_greater_equal_2)
+MPLLIBS_ADD_TEST(suite, test_greater_equal_3)
+
---
+++ src/sample/boost_mpl_unit_test/is_sequence.cpp
@@ -0,0 +1,51 @@
+// Copyright Endre Tamas SAJO (baja@inf.elte.hu) 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_2_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metatest/test.h>
+
+#include <boost/mpl/is_sequence.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/range_c.hpp>
+
+namespace
+{
+  const mpllibs::metatest::TestSuite suite ("is_sequence");
+
+  using namespace boost::mpl;
+
+  template<typename T> struct std_vector
+  {
+    T* begin();
+  };
+
+  struct UDT {};
+
+  typedef is_sequence< std_vector<int> > test1;
+  typedef is_sequence< int_<0> > test2;
+  typedef is_sequence< int > test3;
+  typedef is_sequence< int& > test4;
+  typedef is_sequence< UDT > test5;
+  typedef is_sequence< UDT* > test6;
+  typedef is_sequence< range_c<int,0,0> > test7;
+  typedef is_sequence< list<> > test8;
+  typedef is_sequence< list<int> > test9;
+  typedef is_sequence< vector<> > test10;
+  typedef is_sequence< vector<int> > test11;
+}
+
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test1)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test2)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test3)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test4)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test5)
+MPLLIBS_ADD_TEST_TO_FAIL(suite, test6)
+MPLLIBS_ADD_TEST(suite, test7)
+MPLLIBS_ADD_TEST(suite, test8)
+MPLLIBS_ADD_TEST(suite, test9)
+MPLLIBS_ADD_TEST(suite, test10)
+MPLLIBS_ADD_TEST(suite, test11)
+
