30e89b5e8ae776f138585c93b0aece49bf361ec5
Abel Sinkovics <abelsoft@freemail.hu>
2011-07-23 10:07:58 +0200

    Adding TRY

---
+++ libs/metamonad/test/try.cpp
@@ -0,0 +1,168 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metamonad/try.hpp>
+#include <mpllibs/metamonad/throw.hpp>
+#include <mpllibs/metamonad/get_data.hpp>
+#include <mpllibs/metamonad/tag_tag.hpp>
+#include <mpllibs/metamonad/meta_atom.hpp>
+
+#include <mpllibs/metatest/test.hpp>
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/always.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/plus.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+
+#include "common.hpp"
+
+using boost::mpl::equal_to;
+using boost::mpl::identity;
+using boost::mpl::tag;
+using boost::mpl::plus;
+
+using mpllibs::metatest::suite_path;
+
+using mpllibs::metamonad::exception;
+using mpllibs::metamonad::catch_any;
+
+namespace
+{
+  const suite_path suite("try");
+  
+  MPLLIBS_DEFINE_TAG(tag1)
+  MPLLIBS_DEFINE_META_ATOM(tag1, e1)
+  
+  MPLLIBS_DEFINE_TAG(tag2)
+  MPLLIBS_DEFINE_META_ATOM(tag2, e2)
+
+  typedef
+    equal_to<
+      int13,
+      MPLLIBS_TRY<
+        int13
+      >
+      ::catch_<tag1, x>
+        ::apply<int11>
+      ::type
+    >
+    test_no_exception;
+
+  typedef
+    equal_to<
+      int11,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<e1>
+      >
+      ::catch_<tag1, x>
+        ::apply<identity<int11> >
+      ::type
+    >
+    test_catch;
+
+  typedef
+    equal_to<
+      int13,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<int13>
+      >
+      ::catch_<tag<int13>::type, x>
+        ::apply<identity<x> >
+      ::type
+    >
+    test_exception_value_in_catch;
+
+  typedef
+    equal_to<
+      exception<int13>,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<int13>
+      >
+      ::catch_<tag2, x>
+        ::apply<identity<int11> >
+      ::type
+    >
+    test_not_catching;
+
+  typedef
+    equal_to<
+      int13,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<e2>
+      >
+      ::catch_<tag1, x>
+        ::apply<identity<int11> >
+      ::catch_<tag2, x>
+        ::apply<identity<int13> >
+      ::type
+    >
+    test_second_catch;
+
+  typedef
+    equal_to<
+      MPLLIBS_THROW<int2>,
+      MPLLIBS_TRY<
+        plus<int1, MPLLIBS_THROW<int2> >
+      >
+      ::catch_<tag1, x>
+        ::apply<identity<int11> >
+      ::type
+    >
+    test_exception_propagation;
+
+
+  typedef
+    equal_to<
+      int13,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<e1>
+      >
+      ::catch_<catch_any, x>
+        ::apply<identity<int13> >
+      ::type
+    >
+    test_catch_any;
+
+  typedef
+    equal_to<
+      exception<int13>,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<e1>
+      >
+      ::catch_<catch_any, x>
+        ::apply<MPLLIBS_THROW<int13> >
+      ::type
+    >
+    test_rethrowing;
+
+  typedef
+    equal_to<
+      exception<int13>,
+      MPLLIBS_TRY<
+        MPLLIBS_THROW<e1>
+      >
+      ::catch_<catch_any, x>
+        ::apply<MPLLIBS_THROW<int13> >
+      ::catch_<catch_any, x>
+        ::apply<MPLLIBS_THROW<int13> >
+      ::type
+    >
+    test_rethrowing_not_caught_by_next_catch;
+}
+
+MPLLIBS_ADD_TEST(suite, test_no_exception)
+MPLLIBS_ADD_TEST(suite, test_catch)
+MPLLIBS_ADD_TEST(suite, test_exception_value_in_catch)
+MPLLIBS_ADD_TEST(suite, test_not_catching)
+MPLLIBS_ADD_TEST(suite, test_second_catch)
+MPLLIBS_ADD_TEST(suite, test_exception_propagation)
+MPLLIBS_ADD_TEST(suite, test_catch_any)
+MPLLIBS_ADD_TEST(suite, test_rethrowing)
+MPLLIBS_ADD_TEST(suite, test_rethrowing_not_caught_by_next_catch)
+
+
---
+++ mpllibs/metamonad/try.hpp
@@ -0,0 +1,82 @@
+#ifndef MPLLIBS_METAMONAD_TRY_HPP
+#define MPLLIBS_METAMONAD_TRY_HPP
+
+// Copyright Abel Sinkovics (abel@sinkovics.hu)  2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metamonad/util/id.hpp>
+
+#include <mpllibs/metamonad/do_try.hpp>
+#include <mpllibs/metamonad/do_.hpp>
+
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/comma_if.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+
+namespace mpllibs
+{
+  namespace metamonad
+  {
+    #ifdef MPLLIBS_DEFINE_NAME
+      #error MPLLIBS_DEFINE_NAME already defined
+    #endif
+    #define MPLLIBS_DEFINE_NAME(z, n, unused) \
+      struct t##n;
+
+    BOOST_PP_REPEAT(MPLLIBS_DO_MAX_ARGUMENT, MPLLIBS_DEFINE_NAME, ~)
+    
+    #undef MPLLBIS_DEFINE_NAME
+  
+    template <class F>
+    struct try_ : MPLLIBS_DO_TRY<F> {};
+    
+    #ifdef MPLLIBS_CLASS
+      #error MPLLIBS_CLASS already defined
+    #endif
+    #define MPLLIBS_CLASS(z, n, unused) \
+      BOOST_PP_COMMA_IF(n) class
+    
+    #ifdef MPLLIBS_SET_T
+      #error MPLLIBS_SET_T already defined
+    #endif
+    #define MPLLIBS_SET_T(z, n, unused) \
+      BOOST_PP_COMMA_IF(n) MPLLIBS_SET<t##n, T##n>
+    
+    #ifdef MPLLIBS_TRY_CASE
+      #error MPLLIBS_TRY_CASE already defined
+    #endif
+    #define MPLLIBS_TRY_CASE(z, n, unused) \
+      template <  \
+        template <BOOST_PP_REPEAT(n, MPLLIBS_CLASS, ~)> class F, \
+        BOOST_PP_ENUM_PARAMS(n, class T) \
+      > \
+      struct try_<F<BOOST_PP_ENUM_PARAMS(n, T)> > : \
+        MPLLIBS_DO_TRY< \
+          BOOST_PP_REPEAT(n, MPLLIBS_SET_T, ~) \
+          BOOST_PP_COMMA_IF(n) \
+          F<BOOST_PP_ENUM_PARAMS(n, t)> \
+        > \
+      {};
+    
+    BOOST_PP_REPEAT_FROM_TO(
+      1,
+      BOOST_PP_SUB(MPLLIBS_DO_MAX_ARGUMENT, 1),
+      MPLLIBS_TRY_CASE,
+      ~
+    )
+    
+    #undef MPLLIBS_SET_T
+    #undef MPLLIBS_TRY_CASE
+    #undef MPLLIBS_CLASS
+    
+    #ifdef MPLLIBS_TRY
+      #error MPLLIBS_TRY already defined
+    #endif
+    #define MPLLIBS_TRY mpllibs::metamonad::try_
+  }
+}
+
+#endif
+
