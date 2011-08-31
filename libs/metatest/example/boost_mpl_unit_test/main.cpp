77cbf43d9f80bb5d026392fc2f9e803efe247033
Abel Sinkovics <abelsoft@freemail.hu>
2011-08-31 19:10:04 +0200

    Test suite hierarchy in HTML report

--- libs/metatest/example/boost_mpl_unit_test/main.cpp
+++ libs/metatest/example/boost_mpl_unit_test/main.cpp
@@ -3,15 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-//#include <mpllibs/metatest/main.hpp>
-
-#include <iostream>
-#include <mpllibs/metatest/html_report.hpp>
-int main ()
-{
-  mpllibs::metatest::html_report(std::cout);
-}
-
+#include <mpllibs/metatest/main_html.hpp>
 
 
 
--- libs/metatest/src/html_report.cpp
+++ libs/metatest/src/html_report.cpp
@@ -6,8 +6,10 @@
 #include <mpllibs/metatest/html_report.hpp>
 #include <mpllibs/metatest/test_driver.hpp>
 
-#include <algorithm>
 #include <boost/lambda/lambda.hpp>
+
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,101 +18,105 @@ namespace metatest {
 
 namespace
 {
-  void display(ostream& out_, const string& path_, const test_suite& f_)
+  template <class InputIt>
+  void escaped_output(ostream& out_, InputIt begin_, InputIt end_)
   {
-    for (test_suite::result_list::const_iterator i = f_.results().begin();
-        i != f_.results().end();
-        ++i)
+    for (InputIt c = begin_; c != end_; ++c)
     {
-      out_
-        << "<p class=\"caseName\">"
-        << path_ << i->get_name()
-        << (i->success() ? " <span class=\"ok\">OK</span>"
-                         : " <span class=\"fail\">FAILED</span>")
-        << "</p>";
-
-      out_ << "<div class=\"caseDetails\">";
-
-      if (i->has_reason())
+      switch (*c)
       {
-        const string &reason = i->get_reason();
-
-        for (string::const_iterator c = reason.begin();
-            c != reason.end();
-            ++c)
-        {
-          switch (*c)
-          {
-            case '<': out_ << "&lt;"; break;
-            case '>': out_ << "&gt;"; break;
-            case '&': out_ << "&amp;"; break;
-            default: out_ << *c;
-          }
-        }
+        case '<': out_ << "&lt;"; break;
+        case '>': out_ << "&gt;"; break;
+        case '&': out_ << "&amp;"; break;
+        default: out_ << *c;
       }
-
-      for (test_suite::suite_map::const_iterator s = f_.suites().begin();
-          s != f_.suites().end();
-          ++s)
-      {
-        display(out_, path_ + s->first + "::", s->second);
-      }
-
-      out_ << "</div>";
     }
   }
+  
+  class indent
+  {
+  public:
+    explicit indent(int depth_) :
+      _s(depth_ * indent_spaces, ' ')
+    {}
 
-  //void display(ostream& out_, const string& path_, const test_suite& f_)
-  //{
-  //  using std::endl;
-  //  
-  //  for (
-  //    test_suite::suite_map::const_iterator
-  //      i = f_.suites().begin(),
-  //      e = f_.suites().end();
-  //    i != e;
-  //    ++i
-  //  )
-  //  {
-  //    display(out_, path_ + i->first + "::", i->second);
-  //  }
-  //  
-  //  for (
-  //    test_suite::result_list::const_iterator
-  //      i = f_.results().begin(),
-  //      e = f_.results().end();
-  //    i != e;
-  //    ++i
-  //  )
-  //  {
-  //    out_ << "<p class=\"caseName\">"
-  //         <<   path_ << i->get_name()
-  //         <<   (i->success()
-  //                 ? " <span class=\"ok\">OK</span>"
-  //                 : " <span class=\"fail\">FAIL</span>")
-  //         << "</p>"
-  //    ;
+    indent operator+(int n_) const
+    {
+      return indent(_s + string(n_ * indent_spaces, ' '));
+    }
 
-  //    out_ << "<div class=\"caseDetails\">";
+    friend ostream& operator<<(ostream& out_, indent i_)
+    {
+      return out_ << i_._s;
+    }
+  private:
+    static const int indent_spaces = 2;
+    string _s;
 
-  //    if (i->has_reason())
-  //    {
-  //      const std::string &reason = i->get_reason();
+    explicit indent(string s_) :
+      _s(s_)
+    {}
+  };
 
-  //      for (std::string::const_iterator it = reason.begin();
-  //          it != reason.end();
-  //          ++it)
-  //      {
-  //        if ('<' == *it)      out_ << "&lt;";
-  //        else if ('>' == *it) out_ << "&gt;";
-  //        else if ('&' == *it) out_ << "&amp;";
-  //        else out_ << *it;
-  //      }
-  //    }
+  const char* success_or_failure_html(bool success_)
+  {
+    return
+      success_ ?
+        " <span class=\"ok\">OK</span>"
+        : " <span class=\"fail\">FAILED</span>";
+  }
 
-  //    out_ << "</div>";
-  //  }
-  //}
+  void display(
+    ostream& out_,
+    const string& name_,
+    const test_suite& f_,
+    indent indent_
+  )
+  {
+    const indent i(indent_);
+    const indent ii(name_ == "" ? indent_ : indent_ + 1);
+
+    if (name_ != "")
+    {
+      out_ << i << "<p class=\"caseName\">";
+      escaped_output(out_, name_.begin(), name_.end());
+      out_
+        << success_or_failure_html(f_.failure_count() == 0) << "</p>\n"
+        << i << "<div class=\"caseDetails\">\n"
+      ;
+    }
+    
+    for (test_suite::suite_map::const_iterator s = f_.suites().begin();
+        s != f_.suites().end();
+        ++s)
+    {
+      display(out_, s->first, s->second, ii);
+    }
+
+    for (test_suite::result_list::const_iterator j = f_.results().begin();
+        j != f_.results().end();
+        ++j)
+    {
+      out_ << ii << "<p class=\"caseName\">";
+      escaped_output(out_, j->get_name().begin(), j->get_name().end());
+      out_ << success_or_failure_html(j->success()) << "</p>\n";
+
+      out_ << ii << "<div class=\"caseDetails\">";
+
+      if (j->has_reason())
+      {
+        const string &reason = j->get_reason();
+        escaped_output(out_, reason.begin(), reason.end());
+      }
+
+      out_ << "</div>\n";
+    }
+
+    if (name_ != "")
+    {
+      out_ << i << "</div>\n";
+    }
+  }
 }
 
 bool html_report(std::ostream& out_)
@@ -118,48 +124,51 @@ bool html_report(std::ostream& out_)
   using std::endl;
 
   const test_suite& suite = test_driver::instance().suite();
-  const std::string jquery = "jquery.js";
+  const string jquery = "jquery.js";
 
-  out_ << "<!doctype html>"
-          "<html>"
-            "<head>"
-              "<title>Metatest report</title>"
-              "<script src=\"" << jquery << "\"></script>"
-              "<script>"
-                "$(document).ready(function() {"
-                  "$(\"div.caseDetails\").toggle();"
-                  "$(\"p.caseName\").click(function() {"
-                    "$(this).next(\"div.caseDetails\").slideToggle();"
-                  "});"
-                "});"
-              "</script>"
-              "<style type=\"text/css\">"
-                "<!--"
-                  "p.caseName {"
-                    "font-family:monospace;"
-                    "font-size:120%;"
-                    "cursor:pointer;"
-                    "background:#ddd;"
-                  "} "
-                  "p.caseName:hover { text-decoration:underline; } "
-                  "span.ok { color:#0c0; } "
-                  "span.fail { color:#f00; } "
-                  "div.caseDetails {"
-                    "font-family:monospace;"
-                    "font-size:120%;"
-                    "margin-left:30px"
-                  "}"
-                "-->"
-              "</style>"
-            "</head>"
-            "<body>"
-              "<h1>Metatest report</h1>"
-    ;
+  const indent i(1);
+  const indent ii(2);
+  const indent iii(3);
+  const indent iiii(4);
 
-  display(out_, "", suite);
+  out_
+    <<     "<!doctype html>\n"
+           "<html>\n"
+    << i << "<head>\n"
+    << i << "<title>Metatest report</title>\n"
+    << i << "<script src=\"" << jquery << "\"></script>\n"
+    << i << "<script>" "<!--\n"
+    << ii << "$(document).ready(function() {\n"
+    << iii << "$(\"div.caseDetails\").toggle();\n"
+    << iii << "$(\"p.caseName\").click(function() {\n"
+    << iiii << "$(this).next(\"div.caseDetails\").slideToggle();\n"
+    << iii << "});\n"
+    << ii << "});\n"
+    << i << "-->" "</script>\n"
+    << i << "<style type=\"text/css\">" "<!--\n"
+    << ii << "p.caseName {\n"
+    << iii << "font-family:monospace;\n"
+    << iii << "cursor:pointer;\n"
+    << iii << "background:#ddd;\n"
+    << ii << "}\n"
+    << ii << "p.caseName:hover { text-decoration:underline; }\n"
+    << ii << "span.ok { color:#0c0; }\n"
+    << ii << "span.fail { color:#f00; }\n"
+    << ii << "div.caseDetails {\n"
+    << iii << "font-family:monospace;\n"
+    << iii << "margin-left:30px\n"
+    << ii << "}\n"
+    << i << "-->" "</style>\n"
+    <<     "</head>\n"
+           "<body>\n"
+    << i << "<h1>Metatest report</h1>\n"
+  ;
 
-  out_ <<   "</body>"
-          "</html>"
+  display(out_, "", suite, i);
+
+  out_
+    << "</body>\n"
+       "</html>\n"
     ;
 
   return suite.failure_count() == 0;
