b8a6dfd05fef84975c5158a405eff283cf46f14b
Abel Sinkovics <abelsoft@freemail.hu>
2011-09-19 22:58:07 +0200

    Adding test_result::build

--- mpllibs/metatest/test_driver.hpp
+++ mpllibs/metatest/test_driver.hpp
@@ -6,11 +6,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <mpllibs/metatest/has_type_value.hpp>
-#include <mpllibs/metatest/get_type_value.hpp>
-#include <mpllibs/metatest/has_type.hpp>
 #include <mpllibs/metatest/test_result.hpp>
-#include <mpllibs/metatest/to_stream.hpp>
 #include <mpllibs/metatest/test_suite.hpp>
 
 #include <boost/mpl/equal_to.hpp>
@@ -18,8 +14,6 @@
 #include <boost/type_traits/is_same.hpp>
 
 #include <list>
-#include <iostream>
-#include <sstream>
 
 namespace mpllibs
 {
@@ -28,42 +22,14 @@ namespace mpllibs
     class test_driver
     {
     public:
-      template <class TestFunctor>
+      template <class Pred>
       static void run_test(
         const suite_path& suite_,
         const std::string& name_,
         const location& location_
       )
       {
-        using std::ostringstream;
-        using std::string;
-        
-        const bool has_t = has_type<TestFunctor>::type::value;
-        
-        const bool has_v = has_type_value<TestFunctor, bool>::type::value;
-          
-        const bool result =
-          get_type_value<TestFunctor, boost::mpl::false_>::type::value;
-        
-        ostringstream s;
-        to_stream<TestFunctor>::run(s);
-
-        const bool success = has_t && has_v && result;
-
-        const string reason =
-          has_t ?
-            (
-              has_v ?
-                s.str() :
-                string("Result of test case has no nested boolean value")
-            )
-            :
-            string("Test case has no nested type");
-        
-        test_driver::add(
-          suite_,
-          test_result(name_, location_, success, reason)
-        );
+        test_driver::add(suite_, test_result::build<Pred>(name_, location_));
       }
    
       static test_driver& instance();
--- mpllibs/metatest/test_result.hpp
+++ mpllibs/metatest/test_result.hpp
@@ -7,6 +7,13 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <mpllibs/metatest/location.hpp>
+#include <mpllibs/metatest/has_type_value.hpp>
+#include <mpllibs/metatest/get_type_value.hpp>
+#include <mpllibs/metatest/has_type.hpp>
+#include <mpllibs/metatest/to_stream.hpp>
+
+#include <iostream>
+#include <sstream>
 
 namespace mpllibs
 {
@@ -30,6 +37,38 @@ namespace mpllibs
       const std::string& get_reason() const;
       
       bool has_reason() const;
+      
+      template <class Pred>
+      static test_result build(
+        const std::string& name_,
+        const location& location_
+      )
+      {
+        using std::ostringstream;
+        using std::string;
+        using boost::mpl::false_;
+        
+        const bool has_t = has_type<Pred>::type::value;
+        const bool has_v = has_type_value<Pred, bool>::type::value;
+        const bool result = get_type_value<Pred, false_>::type::value;
+        
+        ostringstream s;
+        to_stream<Pred>::run(s);
+
+        const bool success = has_t && has_v && result;
+
+        const string reason =
+          has_t ?
+            (
+              has_v ?
+                s.str() :
+                string("Result of test case has no nested boolean value")
+            )
+            :
+            string("Test case has no nested type");
+        
+        return test_result(name_, location_, success, reason);
+      }
     private:
       std::string _name;
       location _location;
