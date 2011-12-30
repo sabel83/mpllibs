485844338935391fa99b80c6a4423324280ea5d0
Abel Sinkovics <abelsoft@freemail.hu>
2011-12-30 12:20:37 +0100

    Refactor one_char_except unit tests

--- libs/metaparse/test/one_char_except_c.cpp
+++ libs/metaparse/test/one_char_except_c.cpp
@@ -12,26 +12,7 @@ namespace
   typedef mpllibs::metaparse::one_char_except_c<'0', '1'> oc;
 }
 
-#include <mpllibs/metatest/boost_test.hpp>
-#include <mpllibs/metaparse/source_position.hpp>
-#include <boost/mpl/apply_wrap.hpp>
-
-BOOST_AUTO_TEST_CASE(test_one_char_except_except_c)
-{
-  using mpllibs::metatest::meta_require;
-
-  using mpllibs::metaparse::is_error;
-  using mpllibs::metaparse::start;
-
-  using boost::mpl::apply_wrap2;
-  
-  meta_require<
-    is_error<apply_wrap2<oc, str_0, start> >
-  >(MPLLIBS_HERE, "rejects_except_char");
-}
-
 #define TEST_NAME test_one_char_except_c
 
-#include "one_char_test.hpp"
-
+#include "one_char_except_test.hpp"
 
