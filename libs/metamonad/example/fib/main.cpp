a497cc2cb0c061937104a30249744b7b82cb5c6f
Abel Sinkovics <abelsoft@freemail.hu>
2012-11-01 18:42:55 +0100

    Adding metamonad::lazy

---
+++ libs/metamonad/example/fib/main.cpp
@@ -0,0 +1,80 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metamonad/lazy.hpp>
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/plus.hpp>
+#include <boost/mpl/minus.hpp>
+#include <boost/mpl/less.hpp>
+#include <boost/mpl/eval_if.hpp>
+
+using boost::mpl::plus;
+using boost::mpl::minus;
+using boost::mpl::less;
+using boost::mpl::eval_if;
+using boost::mpl::int_;
+
+using mpllibs::metamonad::lazy;
+
+// Without lazy
+///////////////
+
+template <class N>
+struct fib_without_lazy;
+
+template <class N>
+struct fib_without_lazy_helper :
+  plus<
+    typename fib_without_lazy<typename minus<N, int_<1> >::type>::type,
+    typename fib_without_lazy<typename minus<N, int_<2> >::type>::type
+  >
+{};
+
+template <class N>
+struct fib_without_lazy :
+  eval_if<
+    typename less<N, int_<2> >::type,
+    int_<1>,
+    fib_without_lazy_helper<N>
+  >
+{};
+
+// With lazy
+////////////
+
+template <class N>
+struct fib :
+  eval_if<
+    typename less<N, int_<2> >::type,
+    int_<1>,
+    lazy<plus<fib<minus<N, int_<1> > >, fib<minus<N, int_<2> > > > >
+  >
+{};
+
+///////////
+
+int main()
+{
+  using std::endl;
+
+  std::cout
+    << "With lazy: " << endl
+    << "fib(0) == " << fib_without_lazy<int_<0> >::type::value << endl
+    << "fib(1) == " << fib_without_lazy<int_<1> >::type::value << endl
+    << "fib(2) == " << fib_without_lazy<int_<2> >::type::value << endl
+    << "fib(3) == " << fib_without_lazy<int_<3> >::type::value << endl
+    << "fib(4) == " << fib_without_lazy<int_<4> >::type::value << endl
+    << "fib(5) == " << fib_without_lazy<int_<5> >::type::value << endl
+    << endl
+    << "With lazy: " << endl
+    << "fib(0) == " << fib<int_<0> >::type::value << endl
+    << "fib(1) == " << fib<int_<1> >::type::value << endl
+    << "fib(2) == " << fib<int_<2> >::type::value << endl
+    << "fib(3) == " << fib<int_<3> >::type::value << endl
+    << "fib(4) == " << fib<int_<4> >::type::value << endl
+    << "fib(5) == " << fib<int_<5> >::type::value << endl;
+}
+
---
+++ libs/metamonad/test/lazy.cpp
@@ -0,0 +1,73 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_TEST_DYN_LINK
+
+#include <mpllibs/metamonad/lazy.hpp>
+
+#include <mpllibs/metatest/boost_test.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "common.hpp"
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/divides.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/bool.hpp>
+
+namespace
+{
+  struct returns13
+  {
+    typedef int13 type;
+  };
+
+  template <class A, class B>
+  struct non_lazy_plus : boost::mpl::int_<A::value + B::value> {};
+
+  template <class T>
+  struct always13 : returns13 {};
+}
+
+BOOST_AUTO_TEST_CASE(test_lazy)
+{
+  using mpllibs::metatest::meta_require;
+
+  using mpllibs::metamonad::lazy;
+  
+  using boost::mpl::equal_to;
+  using boost::mpl::divides;
+  using boost::mpl::eval_if;
+  using boost::mpl::if_;
+  using boost::mpl::false_;
+
+  typedef divides<int1, int0> breaking_expr;
+
+  meta_require<
+    equal_to<int13, lazy<divides<int26, int2> >::type>
+  >(MPLLIBS_HERE, "test_evaluation");
+
+  meta_require<
+    equal_to<int26, lazy<non_lazy_plus<returns13, returns13> >::type>
+  >(MPLLIBS_HERE, "test_lazyness");
+
+  meta_require<
+    equal_to<
+      int13,
+      lazy<eval_if<false_, breaking_expr, divides<int26, int2> > >::type
+    >
+  >(MPLLIBS_HERE, "test_eval_if");
+
+  meta_require<
+    equal_to<
+      int13,
+      lazy<if_<false_, breaking_expr, divides<int26, int2> > >::type::type
+    >
+  >(MPLLIBS_HERE, "test_if");
+}
+
+
