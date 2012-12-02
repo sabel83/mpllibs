0c356f3092394c424c52fe0458c2db29b9f30f88
Abel Sinkovics <abelsoft@freemail.hu>
2012-12-02 12:52:04 +0100

    Fix some special cases for lazy

--- libs/metamonad/test/lazy.cpp
+++ libs/metamonad/test/lazy.cpp
@@ -31,6 +31,17 @@ namespace
 
   template <class T>
   struct always13 : returns13 {};
+
+  struct hidden_result
+  {
+    typedef int13 the_result;
+    // no ::type
+  };
+
+  struct can_be_evaluated_only_once
+  {
+    typedef hidden_result type;
+  };
 }
 
 BOOST_AUTO_TEST_CASE(test_lazy)
@@ -68,6 +79,32 @@ BOOST_AUTO_TEST_CASE(test_lazy)
       lazy<if_<false_, breaking_expr, divides<int26, int2> > >::type::type
     >
   >(MPLLIBS_HERE, "test_if");
+
+  meta_require<
+    equal_to<
+      int26,
+      lazy<
+        eval_if<false_, breaking_expr, non_lazy_plus<returns13, returns13> >
+      >::type
+    >
+  >(MPLLIBS_HERE, "test_eval_if_with_lazy_expression_as_selected_case");
+
+  meta_require<
+    equal_to<
+      int26,
+      lazy<
+        if_<false_, breaking_expr, non_lazy_plus<returns13, returns13> >
+      >::type::type
+    >
+  >(MPLLIBS_HERE, "test_if_with_lazy_expression_as_selected_case");
+
+  meta_require<
+    equal_to<int13, lazy<can_be_evaluated_only_once>::type::the_result>
+  >(MPLLIBS_HERE, "test_evaluation_limit");
+
+  meta_require<
+    equal_to<int13, lazy<lazy<can_be_evaluated_only_once> >::type::the_result>
+  >(MPLLIBS_HERE, "test_evaluation_limit_of_double_lazy");
 }
 
 
