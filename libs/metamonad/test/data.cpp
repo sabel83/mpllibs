9cc920e0c3262f825f814e55e94ed65875eb308d
Abel Sinkovics <abelsoft@freemail.hu>
2012-12-22 20:10:23 +0100

    Macro for algebraic data-type definition

---
+++ libs/metamonad/test/data.cpp
@@ -0,0 +1,76 @@
+// Copyright Abel Sinkovics (abel@sinkovics.hu) 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_TEST_DYN_LINK
+
+#include <mpllibs/metamonad/data.hpp>
+#include <mpllibs/metamonad/lazy_metafunction.hpp>
+
+#include <mpllibs/metatest/boost_test.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <boost/type_traits.hpp>
+
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/not.hpp>
+
+namespace
+{
+  namespace bar
+  {
+    MPLLIBS_DATA(foo, ((con0, 0))((con1, 1))((con2, 1)));
+  }
+  
+  MPLLIBS_LAZY_METAFUNCTION(lazy_not, (E)) ((boost::mpl::not_<E>));
+}
+
+MPLLIBS_DEFINE_EQUALITY(bar::foo)
+
+BOOST_AUTO_TEST_CASE(test_data)
+{
+  using mpllibs::metatest::meta_require;
+
+  using boost::is_same;
+
+  using boost::mpl::equal_to;
+  using boost::mpl::int_;
+
+  using namespace bar;
+  
+  meta_require<
+    is_same<foo_tag, con0::tag>
+  >(MPLLIBS_HERE, "test_tag_of_nullary_constructor");
+
+  meta_require<
+    is_same<foo_tag, con1<int>::tag>
+  >(MPLLIBS_HERE, "test_tag_of_unary_constructor");
+
+  meta_require<
+    is_same<con0, con0::type>
+  >(MPLLIBS_HERE, "test_type_of_nullary_constructor");
+
+  meta_require<
+    is_same<con1<int>, con1<int>::type>
+  >(MPLLIBS_HERE, "test_type_of_unary_constructor");
+
+  meta_require<
+    equal_to<con1<int_<1> >, con1<int_<1> > >
+  >(MPLLIBS_HERE, "test_equality");
+
+  meta_require<
+    lazy_not<equal_to<con1<int_<1> >, con1<int_<2> > > >
+  >(MPLLIBS_HERE, "test_different_constructor_args");
+
+  meta_require<
+    lazy_not<equal_to<con1<int_<1> >, con2<int_<1> > > >
+  >(MPLLIBS_HERE, "test_different_constructors");
+
+  meta_require<
+    equal_to<con0, con0>
+  >(MPLLIBS_HERE, "test_nullary_constructor_equality");
+}
+
+
---
+++ mpllibs/metamonad/data.hpp
@@ -0,0 +1,153 @@
+#ifndef MPLLIBS_METAMONAD_DATA_HPP
+#define MPLLIBS_METAMONAD_DATA_HPP
+
+// Copyright Abel Sinkovics (abel@sinkovics.hu)  2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <mpllibs/metamonad/tmp_tag.hpp>
+#include <mpllibs/metamonad/lazy_metafunction.hpp>
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/control/if.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/eat.hpp>
+#include <boost/preprocessor/repetition/enum.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+
+#include <boost/mpl/always.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/equal.hpp>
+
+#include <boost/type_traits.hpp>
+
+#ifndef MPLLIBS_DATA_MAX_TEMPLATE_ARGUMENT
+  #define MPLLIBS_DATA_MAX_TEMPLATE_ARGUMENT 10
+#endif
+
+#ifdef MPLLIBS_DATA_CONSTR_ARGS
+  #error MPLLIBS_DATA_CONSTR_ARGS already defined
+#endif
+#define MPLLIBS_DATA_CONSTR_ARGS(arity) \
+  template <BOOST_PP_ENUM(arity, class BOOST_PP_TUPLE_EAT(3), ~)>
+
+#ifdef MPLLIBS_DATA_CONSTR
+  #error MPLLIBS_DATA_CONSTR already defined
+#endif
+#define MPLLIBS_DATA_CONSTR(tag_name, name, arity) \
+  BOOST_PP_IF(arity, MPLLIBS_DATA_CONSTR_ARGS, BOOST_PP_TUPLE_EAT(1))(arity) \
+  struct name \
+  { \
+    typedef name type; \
+    typedef tag_name tag; \
+  };
+
+#ifdef MPLLIBS_DATA_CONSTR_CB
+  #error MPLLIBS_DATA_CONSTR_CB already defined
+#endif
+#define MPLLIBS_DATA_CONSTR_CB(r, tag_name, constr) \
+  MPLLIBS_DATA_CONSTR( \
+    tag_name, \
+    BOOST_PP_TUPLE_ELEM(2, 0, constr), \
+    BOOST_PP_TUPLE_ELEM(2, 1, constr) \
+  )
+
+#ifdef MPLLIBS_DATA
+  #error MPLLIBS_DATA already defined
+#endif
+#define MPLLIBS_DATA(name, constrs) \
+  struct BOOST_PP_CAT(name, _tag); \
+  \
+  BOOST_PP_SEQ_FOR_EACH( \
+    MPLLIBS_DATA_CONSTR_CB, \
+    BOOST_PP_CAT(name, _tag), \
+    constrs \
+  ) \
+  \
+  struct BOOST_PP_CAT(name, _tag) : \
+    mpllibs::metamonad::tmp_tag<BOOST_PP_CAT(name, _tag)> \
+  {}
+
+namespace mpllibs
+{
+  namespace metamonad
+  {
+    namespace impl
+    {
+      template <class A, class B>
+      struct rec_equal_to_impl : boost::mpl::false_ {};
+
+      #ifdef REC_EQUAL_TO_CASE
+        #error REC_EQUAL_TO_CASE already defined
+      #endif
+      #define REC_EQUAL_TO_CASE(z, n, unused) \
+        template < \
+          MPLLIBS_DATA_CONSTR_ARGS(n) class T, \
+          BOOST_PP_ENUM_PARAMS(n, class A), \
+          BOOST_PP_ENUM_PARAMS(n, class B) \
+        > \
+        struct \
+          rec_equal_to_impl< \
+            T<BOOST_PP_ENUM_PARAMS(n, A)>, \
+            T<BOOST_PP_ENUM_PARAMS(n, B)> \
+          > : \
+          boost::mpl::equal< \
+            boost::mpl::vector<BOOST_PP_ENUM_PARAMS(n, A)>, \
+            boost::mpl::vector<BOOST_PP_ENUM_PARAMS(n, B)> \
+          > \
+        {};
+      
+      BOOST_PP_REPEAT_FROM_TO(
+        1,
+        MPLLIBS_DATA_MAX_TEMPLATE_ARGUMENT,
+        REC_EQUAL_TO_CASE,
+        ~
+      )
+      
+      #undef REC_EQUAL_TO_CASE
+
+      // It detects when the same constructor is used for both values and
+      // compares the constructor arguments using equal_to
+      template <class A, class B>
+      struct rec_equal_to : rec_equal_to_impl<A, B> {};
+
+      template <class A>
+      struct rec_equal_to<A, A> : boost::mpl::true_ {};
+    }
+  }
+}
+
+#ifdef MPLLIBS_DEFINE_EQUALITY
+  #error MPLLIBS_DEFINE_EQUALITY already defined
+#endif
+#define MPLLIBS_DEFINE_EQUALITY(name) \
+  namespace boost \
+  { \
+    namespace mpl \
+    { \
+      template <class, class> \
+      struct equal_to_impl; \
+      \
+      template <class T> \
+      struct equal_to_impl<T, BOOST_PP_CAT(name, _tag)> : always<false_> {}; \
+      \
+      template <class T> \
+      struct equal_to_impl<BOOST_PP_CAT(name, _tag), T> : always<false_> {}; \
+      \
+      template <> \
+      struct equal_to_impl<BOOST_PP_CAT(name, _tag), BOOST_PP_CAT(name, _tag)> \
+      { \
+        typedef equal_to_impl type; \
+        \
+        MPLLIBS_LAZY_METAFUNCTION(apply, (A)(B)) \
+        ((mpllibs::metamonad::impl::rec_equal_to<A, B>)); \
+      }; \
+    } \
+  }
+
+#endif
+
