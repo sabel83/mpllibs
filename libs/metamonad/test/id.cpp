b97bbdae9be5760fed280d815386b96238ff7990
Abel Sinkovics <abelsoft@freemail.hu>
2013-02-01 20:37:33 +0100

    Clean id unit test

--- libs/metamonad/test/id.cpp
+++ libs/metamonad/test/id.cpp
@@ -13,16 +13,14 @@
 #include <boost/mpl/int.hpp>
 #include <boost/mpl/equal_to.hpp>
 
-using boost::mpl::int_;
-using boost::mpl::equal_to;
-
-using mpllibs::metamonad::util::id;
-
-using std::string;
-
 BOOST_AUTO_TEST_CASE(test_util_id)
 {
   using mpllibs::metatest::meta_require;
+
+  using mpllibs::metamonad::util::id;
+
+  using boost::mpl::equal_to;
+  using boost::mpl::int_;
 
   typedef int_<13> int13;
 
