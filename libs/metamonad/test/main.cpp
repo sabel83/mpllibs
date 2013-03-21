a2d6fac2258f879668242975274ae868142335f5
Abel Sinkovics <abelsoft@freemail.hu>
2013-03-21 20:27:35 +0100

    Fix documentation and Metamonad's test module name

--- libs/metamonad/test/main.cpp
+++ libs/metamonad/test/main.cpp
@@ -4,7 +4,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE metaparse
+#define BOOST_TEST_MODULE metamonad
 #include <boost/test/unit_test.hpp>
 
 
