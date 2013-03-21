c9e8128ec07f968e9ba4951fd123ffbc254a731b
Abel Sinkovics <abelsoft@freemail.hu>
2013-03-21 16:27:30 +0100

    Move the exception_debug example to metamonad

--- libs/metamonad/example/exception_debug/main.cpp
+++ libs/metamonad/example/exception_debug/main.cpp
@@ -5,32 +5,28 @@
 
 #include <mpllibs/metamonad/try_c.hpp>
 #include <mpllibs/metamonad/exception.hpp>
-#include <mpllibs/metamonad/tmp_value.hpp>
-
-#include <mpllibs/metatest/debug.hpp>
-#include <mpllibs/metatest/to_stream.hpp>
+#include <mpllibs/metamonad/data.hpp>
+#include <mpllibs/metamonad/value_to_stream.hpp>
 
 #include <boost/mpl/eval_if.hpp>
 #include <boost/mpl/equal_to.hpp>
 #include <boost/mpl/int.hpp>
 #include <boost/mpl/divides.hpp>
 
+#include <iostream>
+
 using boost::mpl::int_;
 using boost::mpl::eval_if;
 using boost::mpl::equal_to;
 using boost::mpl::divides;
 
-using mpllibs::metatest::debug;
-
 using mpllibs::metamonad::try_c;
 using mpllibs::metamonad::exception;
-using mpllibs::metamonad::tmp_value;
 
 typedef int_<0> int0;
 typedef int_<13> int13;
 
-struct division_by_zero : tmp_value<division_by_zero> {};
-MPLLIBS_DEFINE_TO_STREAM_FOR_TYPE(division_by_zero, "division by zero")
+MPLLIBS_DATA(division_by_zero, 0, ((division_by_zero, 0)));
 
 template <class A, class B>
 struct safe_divides :
@@ -43,13 +39,12 @@ struct safe_divides :
   >
 {};
 
-MPLLIBS_DEFINE_TO_STREAM_FOR_TEMPLATE(2, safe_divides, "safe_divides")
-
-debug<safe_divides<int13, int0> > d;
-
 int main()
 {
-  // some other code
+  mpllibs::metamonad::value_to_stream<
+    safe_divides<int13, int0>
+  >::run(std::cout);
+  std::cout << std::endl;
 }
 
 
