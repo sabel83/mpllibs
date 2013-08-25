5175152f6c03ca3e696b3a7700e9bd2af60784c8
Abel Sinkovics <abelsoft@freemail.hu>
2013-08-25 17:47:26 +0200

    Adding CPack support

---
+++ mpllibs/version.hpp
@@ -0,0 +1,24 @@
+#ifndef MPLLIBS_VERSION_HPP
+#define MPLLIBS_VERSION_HPP
+
+// Copyright Abel Sinkovics (abel@sinkovics.hu)  2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifdef MPLLIBS_MAJOR
+  #error MPLLIBS_MAJOR already defined
+#endif
+#ifdef MPLLIBS_MINOR
+  #error MPLLIBS_MINOR already defined
+#endif
+#ifdef MPLLIBS_PATCH
+  #error MPLLIBS_PATCH already defined
+#endif
+
+#define MPLLIBS_MAJOR_VERSION 0
+#define MPLLIBS_MINOR_VERSION 1
+#define MPLLIBS_PATCH_VERSION 0
+
+#endif
+
