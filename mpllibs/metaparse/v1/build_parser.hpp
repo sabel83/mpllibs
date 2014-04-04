3b0d95a542bb019a55acc2bc11f9413dd11ac1ec
Abel Sinkovics <abelsoft@freemail.hu>
2014-04-04 20:09:24 +0200

    Fix: compilation error from build_parser when parsing fails

--- mpllibs/metaparse/v1/build_parser.hpp
+++ mpllibs/metaparse/v1/build_parser.hpp
@@ -9,8 +9,16 @@
 #include <mpllibs/metaparse/v1/fwd/build_parser.hpp>
 #include <mpllibs/metaparse/v1/start.hpp>
 #include <mpllibs/metaparse/v1/get_result.hpp>
+#include <mpllibs/metaparse/v1/get_position.hpp>
+#include <mpllibs/metaparse/v1/get_message.hpp>
+#include <mpllibs/metaparse/v1/get_line.hpp>
+#include <mpllibs/metaparse/v1/get_col.hpp>
+#include <mpllibs/metaparse/v1/is_error.hpp>
 
 #include <boost/mpl/apply.hpp>
+#include <boost/mpl/eval_if.hpp>
+
+#include <boost/static_assert.hpp>
 
 namespace mpllibs
 {
@@ -18,13 +26,33 @@ namespace mpllibs
   {
     namespace v1
     {
+      template <int Line, int Col, class Msg>
+      struct x__________________PARSING_FAILED__________________x
+      {
+        BOOST_STATIC_ASSERT(Line == Line + 1);
+      };
+
       template <class P>
       struct build_parser
       {
         typedef build_parser type;
         
         template <class S>
-        struct apply : get_result<boost::mpl::apply<P, S, start> > {};
+        struct apply :
+          boost::mpl::eval_if<
+            typename is_error<boost::mpl::apply<P, S, start> >::type,
+            x__________________PARSING_FAILED__________________x<
+              get_line<
+                get_position<boost::mpl::apply<P, S, start> >
+              >::type::value,
+              get_col<
+                get_position<boost::mpl::apply<P, S, start> >
+              >::type::value,
+              typename get_message<boost::mpl::apply<P, S, start> >::type
+            >,
+            get_result<boost::mpl::apply<P, S, start> >
+          >
+        {};
       };
     }
   }
